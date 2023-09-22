67426cf308a9960ad914f6461f1a2d739b8a48ed
v-sec0 <122383941+v-sec0@users.noreply.github.com>
2023-09-22 18:55:15 +0000

    Adding correct themeing

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,12 @@
 $(document).ready(function () { 
   let replyMessage = $("#response");
   replyMessage.text("");
-  
+
+   let gamertag = $("#gamertag").val()
+
   $("#btnJudgeUsername")
     .click(function () {
-    replyMessage.text("What's up " + $("#gamertag").val() + ", your gamertag is MAJOR EPIC!  ");
+    replyMessage.text(`The gamertag ${gamertag} is taken. Maybe try ${gamertag}123 or ${gamertag}01.`);
   });
   
 });
